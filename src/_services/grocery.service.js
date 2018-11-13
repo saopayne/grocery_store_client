41076a8b6ae8eb2fc482e7be2c9e59dcca680fc8
Ademola Oyewale <saopayne@gmail.com>
2018-11-13 07:44:33 +0100

    Remove node modules for deployment on Heroku

--- src/_services/grocery.service.js
+++ src/_services/grocery.service.js
@@ -14,7 +14,7 @@ function add(name, quantity, unit) {
         body: JSON.stringify({ name, quantity, unit })
     };
 
-    return fetch(`https://guarded-brook-31463.herokuapp.com/grocerylists/1/items/`, requestOptions)
+    return fetch(`http://127.0.0.1:5000/grocerylists/1/items/`, requestOptions)
         .then(handleResponse)
         .then(item => {
             if (item.token) {
@@ -31,7 +31,7 @@ function update(item) {
         body: JSON.stringify(item)
     };
 
-    return fetch(`https://guarded-brook-31463.herokuapp.com/grocerylists/1/items/${item.id}`, requestOptions)
+    return fetch(`http://127.0.0.1:5000/grocerylists/1/items/${item.id}`, requestOptions)
         .then(handleResponse);
 }
 
@@ -40,7 +40,7 @@ function getAll() {
         method: 'GET'
     };
 
-    return fetch(`https://guarded-brook-31463.herokuapp.com/grocerylists/1/items/`, requestOptions)
+    return fetch(`http://127.0.0.1:5000/grocerylists/1/items/`, requestOptions)
         .then(handleResponse);
 }
 
@@ -50,16 +50,16 @@ function getById(id) {
         mode: 'no-cors'
     };
 
-    return fetch(`https://guarded-brook-31463.herokuapp.com/grocerylists/1/items/${id}`, requestOptions).then(handleResponse);
+    return fetch(`http://127.0.0.1:5000/grocerylists/1/items/${id}`, requestOptions).then(handleResponse);
 }
 
 // prefixed function name with underscore because delete is a reserved word in javascript
 function _delete(id) {
     const requestOptions = {
-        method: 'DELETE'
+        method: 'DELETE',
     };
 
-    return fetch(`https://guarded-brook-31463.herokuapp.com/grocerylists/1/items/${id}`, requestOptions).then(handleResponse);
+    return fetch(`http://127.0.0.1:5000/grocerylists/1/items/${id}`, requestOptions).then(handleResponse);
 }
 
 function register(user) {
@@ -69,7 +69,7 @@ function register(user) {
         body: JSON.stringify(user)
     };
 
-    return fetch(`https://guarded-brook-31463.herokuapp.com/users/register`, requestOptions).then(handleResponse);
+    return fetch(`http://127.0.0.1:5000/users/register`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
